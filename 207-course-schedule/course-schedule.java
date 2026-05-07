b0babc8ddde31233428e7e2ab5e5132bd5e15121
Syed Shajahan <vtu25639@veltech.edu.in>
2026-05-07 22:11:40 +0530

    Time: 7 ms (59.43%) | Memory: 47.2 MB (47.73%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean canFinish(int numCourses,
+                             int[][] prerequisites) {
+
+        List<List<Integer>> graph =
+            new ArrayList<>();
+
+        for(int i=0;i<numCourses;i++)
+            graph.add(new ArrayList<>());
+
+        int[] indegree =
+            new int[numCourses];
+
+        for(int[] p : prerequisites) {
+            graph.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
+        }
+
+        Queue<Integer> q =
+            new LinkedList<>();
+
+        for(int i=0;i<numCourses;i++) {
+            if(indegree[i] == 0)
+                q.offer(i);
+        }
+
+        int count = 0;
+
+        while(!q.isEmpty()) {
+
+            int node = q.poll();
+
+            count++;
+
+            for(int nei : graph.get(node)) {
+
+                indegree[nei]--;
+
+                if(indegree[nei] == 0)
+                    q.offer(nei);
+            }
+        }
+
+        return count == numCourses;
+    }
+}

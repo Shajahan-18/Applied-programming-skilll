7f69969f805cba5d46ba74ed311ae9970b1948b7
Syed Shajahan <vtu25639@veltech.edu.in>
2026-05-07 22:38:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (40.81%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int rob(int[] nums) {
+
+        if(nums.length == 1)
+            return nums[0];
+
+        return Math.max(
+            helper(nums,0,nums.length-2),
+            helper(nums,1,nums.length-1)
+        );
+    }
+
+    int helper(int[] nums,
+               int start,
+               int end) {
+
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for(int i=start;i<=end;i++) {
+
+            int temp =
+                Math.max(prev1,
+                         prev2 + nums[i]);
+
+            prev2 = prev1;
+            prev1 = temp;
+        }
+
+        return prev1;
+    }
+}
